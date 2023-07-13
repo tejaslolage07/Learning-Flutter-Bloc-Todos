3ec4ab728f7a2a5ebf237fcc7a434439ac8efb11
Tejas Lolage <tejaslolage@gmail.com>
2023-07-13 11:38:32 +0900

    Created the flutter app using very_good cli

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.todos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:todos/app/app.dart';
+import 'package:todos/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
